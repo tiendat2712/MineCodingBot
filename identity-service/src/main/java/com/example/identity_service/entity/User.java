fa4af6f1c2b5f4e581bb1fd9a16fc0f681453742
datphan <tiendat.xpbp@gmail.com>
2024-05-26 00:02:47 +0700

    SpringMVC-Crud-Exception

---
+++ identity-service/src/main/java/com/example/identity_service/entity/User.java
@@ -0,0 +1,78 @@
+package com.example.identity_service.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+
+    public User() {
+    }
+
+    public User(String id, String username, String password, String firstName, String lastName, LocalDate dob) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+}
---
+++ identity-service/src/main/java/com/example/identity_service/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.identity_service.repository;
+
+import com.example.identity_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByUsername(String username);
+}
